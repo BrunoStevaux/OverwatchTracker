bd444513be2ac1c4c27a518664afcaedc9ec8fb8
Bruno Stevaux <144894s@acadiau.ca>
2022-10-06 23:27:34 -0300

    Update

---
+++ Utilities/fetchPlayer.js
@@ -0,0 +1,9 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+async function getPlayer(playerName) {
+    return (`${playerName} is cool`)
+}
+module.exports = {
+    getPlayer   
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,62 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 
+import { getPlayer } from '../Utilities/fetchPlayer'
+
+const accounts = [...Array(5)]
 export default function Home() {
+
+  const [playerSearch, setPlayerSearch] = useState('');
+  let test = "Hello"
+
+  const handlePlayerSearch = async (e) => {
+    test = await getPlayer(playerSearch)
+    console.log(test)
+  }
+
+  // const getAccounts
+
   return (
     <div className={styles.container}>
-      <input id="pleayer-search-name" type="search" placeholder="PizzaLawyer#11545"></input>
-      <button id="pleayer-search-button">Search</button>
+      <div className="container bg-secondary">
+        <div className="d-flex flex-row">
+          <div className="p-2">
+            <img src="https://blz-contentstack-images.akamaized.net/v3/assets/blt9c12f249ac15c7ec/bltbcf2689c29fa39eb/622906a991f4232f0085d3cc/Masthead_Overwatch2_Logo.png?auto=webp" height="20"/>
+          </div>
+          <div className="p-2">
+            Account Tracker
+          </div>
+        </div>
+      </div>
+
+
+      <input
+        type="search"
+        value={playerSearch}
+        placeholder="PizzaLawyer#11545"
+        onChange={(e) => { setPlayerSearch(e.currentTarget.value) }}
+      ></input>
+      <button
+        type="button"
+        className="btn btn-primary"
+        id="pleayer-search-button"
+        onClick={(e) => handlePlayerSearch(e)}
+      >
+        Search
+      </button>
+
+      <div className="row gap-3">
+        {accounts.map((account, id) => 
+          <div className="col-sm border border-dark rounded" key={id} style={{width: "200px"}}>
+            PizzaLawyer#11545
+            Tank: Master 2
+            Damage: Grandmaster 3
+            Support: Unplaced
+          </div>
+        )}
+      </div>
     </div>
   )
 }
