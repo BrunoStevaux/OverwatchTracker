2efd565e220afd3c31ca5531976257a14950f809
Bruno Stevaux <144894s@acadiau.ca>
2022-11-04 16:02:57 -0300

    Added delete functionality

--- pages/index.js
+++ pages/index.js
@@ -14,14 +14,21 @@ export default function Home() {
   const [playerSearch, setPlayerSearch] = useState('')
 
   const handlePlayerSearchInput = async (e) => {
-    if(playerSearch.length < 1) return
-    setPlayerSearch("")
-    const newAccount = await getPlayer(playerSearch)
-    setShowAccounts(currentAccounts => [... currentAccounts, newAccount])
+    if (playerSearch.length < 1) return // Check if there is any input
+
+
+
+    if (showAccounts.filter(account => account.name == playerSearch).length > 0) { // Check that account is not duplicate
+      alert(`Account "${playerSearch}" already added`)
+      return
+    }
+    addPlayer(playerSearch) // Add player to our list
+    setPlayerSearch("") // Reset the search field
   }
 
-  const search = async (name) => {
-    const newAccount = await getPlayer(name)
+  const addPlayer = async (account) => {
+    const newAccount = await getPlayer(account)
+    setShowAccounts(currentAccounts => [... currentAccounts, newAccount])
   }
 
   const handleEnterKey = (e) => {
@@ -34,10 +41,12 @@ export default function Home() {
     console.log(showAccounts[id])
   }
 
-  const removeAccount = (id) => {
-    let test = showAccounts
-    test.splice(id, 1)
-    setShowAccounts(test)
+  const removeAccount = (account, id) => {
+    console.log(`Removing account [${showAccounts[id].name}] - id ${id}`)    
+    setShowAccounts(showAccounts.filter(account => 
+      account.name !== showAccounts[id].name
+      ))
+    console.log(`Success`)
   }
 
   return (
@@ -64,7 +73,6 @@ export default function Home() {
         ></input>
         <button
           type="button"
-          // value={submit}
           className="btn btn-primary"
           id="player-search-button"
           onClick={(e) => handlePlayerSearchInput(e)}
@@ -77,36 +85,36 @@ export default function Home() {
         >
           
           {showAccounts.map((account, id) => 
-            <div class="card" style={{ width: "320px", margin: "3px" }} id={id} key={id}>
+            <div className="card" style={{ width: "320px", margin: "3px" }} id={id} key={id}>
               {/* <img class="card-img-top" src={account.profileIcon}></img> */}
-              <div class="card-body">
+              <div className="card-body">
                 <div style={{display: "flex"}}>
                   <img src={account.profileIcon} width="40"></img>
-                  <h5 class="card-header">{account.name}</h5>
+                  <h5 className="card-header">{account.name}</h5>
                   <button className="btn btn-success" onClick={(e) => updateAccount(account, id)}> <FontAwesomeIcon icon={faRefresh}/> </button>
-                  <button className="btn btn-danger" onClick={(e) => removeAccount(id)}> <FontAwesomeIcon icon={faClose}/> </button>
+                  <button className="btn btn-danger" onClick={(e) => removeAccount(account, id)}> <FontAwesomeIcon icon={faClose}/> </button>
                 </div>
 
-                <p class="card-text" style = {{color: "darkgrey"}}>Last Updated: {moment(account.lastUpdated).fromNow()}</p>
+                <p className="card-text" style = {{color: "darkgrey"}}>Last Updated: {moment(account.lastUpdated).fromNow()}</p>
                 <div style= {{display: "flex"}}>
                   <button><FontAwesomeIcon icon={faChevronDown}/></button>
                   <button><FontAwesomeIcon icon={faChevronUp}/></button>
-                  <p class="card-text">Tank: {account.tankSR}</p>
-                </div>
-
-                <div style= {{display: "flex"}}>
-                  <button><FontAwesomeIcon icon={faChevronDown}/></button>
-                  <button><FontAwesomeIcon icon={faChevronUp}/></button>
-                  <p class="card-text">Damage: {account.damageSR}</p>
+                  <p className="card-text">Tank: {account.tankSR}</p>
                 </div>
 
                 <div style= {{display: "flex"}}>
                   <button><FontAwesomeIcon icon={faChevronDown}/></button>
                   <button><FontAwesomeIcon icon={faChevronUp}/></button>
-                  <p class="card-text">Support: {account.supportSR}</p>
+                  <p className="card-text">Damage: {account.damageSR}</p>
                 </div>
 
-                <a href="#" class="card-link">View online</a>
+                <div style= {{display: "flex"}}>
+                  <button><FontAwesomeIcon icon={faChevronDown}/></button>
+                  <button><FontAwesomeIcon icon={faChevronUp}/></button>
+                  <p className="card-text">Support: {account.supportSR}</p>
+                </div>
+
+                <a href="#" className="card-link">View online</a>
               </div>
             </div>
           )}
