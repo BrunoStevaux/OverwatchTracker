2f93f730f3d129cc4d1d120a3d26ab4bfacc4e86
Bruno Stevaux <brnstvx@gmail.com>
2023-02-10 10:46:06 -0400

    imports

--- src/pages/api/fetchPlayer.ts
+++ src/pages/api/fetchPlayer.ts
@@ -1,12 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from "next";
 const cheerio = require("cheerio");
 const axios = require("axios");
 
 async function retrieveData(): Promise<any> {
   try {
-    const response = await axios.get('https://overwatch.blizzard.com/en-gb/career/FreyaTheCat-1718/');
+    const response = await axios.get(
+      "https://overwatch.blizzard.com/en-gb/career/FreyaTheCat-1718/"
+    );
     const html = response.data;
     const $ = cheerio.load(html);
-    const imageSource = $('.Profile-player--portrait').attr('src');
+    const imageSource = $(".Profile-player--portrait").attr("src");
     const jsonResponse = { imageSource };
     return jsonResponse;
   } catch (error) {
