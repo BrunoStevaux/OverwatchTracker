a721238f12c92fd95a2c7116c671d28c67fded4c
Bruno Stevaux <144894s@acadiau.ca>
2023-02-10 14:25:54 -0400

    ?

--- src/pages/api/fetchPlayer.ts
+++ src/pages/api/fetchPlayer.ts
@@ -2,35 +2,33 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-async function retrieveData(): Promise<any> {
+interface Rankings {
+  [key: string]: string;
+}
+
+async function retrieveData(): Promise<Rankings> {
   try {
     const response = await axios.get(
-      "https://overwatch.blizzard.com/en-us/career/FreyaTheCat-1718/"
+      "https://overwatch.blizzard.com/en-gb/career/FreyaTheCat-1718/"
     );
     const html = response.data;
     const $ = cheerio.load(html);
-    const imageSource = $(".Profile-player--portrait").attr("src");
-    const rankings = {
-      tank: "",
-      offense: "",
-      support: "",
-    };
     const rankingNodes = $(
       ".mouseKeyboard-view.Profile-playerSummary--rankWrapper.is-active"
     ).find(".Profile-playerSummary--roleWrapper");
+    const rankings: Rankings = {};
     rankingNodes.each((i, elem) => {
       const role = $(elem)
         .find(".Profile-playerSummary--role img")
         .attr("src")
-        .match(/role\/(\w+)/)[1];
+        .match(/role\/(.*)\-.*/)[1];
       const rank = $(elem)
         .find(".Profile-playerSummary--rank")
         .attr("src")
-        .match(/rank\/(\w+-\d+)/)[1];
+        .match(/rank\/(.*)\-.*/)[1];
       rankings[role] = rank;
     });
-    const jsonResponse = { imageSource, rankings };
-    return jsonResponse;
+    return rankings;
   } catch (error) {
     console.error(error);
     return error;
@@ -39,7 +37,7 @@ async function retrieveData(): Promise<any> {
 
 export default async function fetchPlayer(
   req: NextApiRequest,
-  res: NextApiResponse<Response>
+  res: NextApiResponse<Rankings>
 ) {
   res.status(200).json(await retrieveData());
 }
